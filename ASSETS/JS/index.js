279408c65077393cf322490dace6eeddaec22a0b
Alishag94 <griffinalisha01@gmail.com>
2022-12-12 19:45:10 -0500

    updated

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -1,29 +1,26 @@
-const newTaskNameInput = document.querySelector('#task-name');
-const newDescriptionInput = document.querySelector('#description');
-const newAssignedToInput = document.querySelector('#assigned-to');
-const newDueDateInput = document.querySelector('#due-date');
-
-const taskManager = new taskManager()
+const taskManager = new TaskManager()
 taskManager.load()
 taskManager.render()
-
 const validFormFieldInput = () => {
-    
+    const newTaskNameInput = document.querySelector('#task-name')   ;
     const name = newTaskNameInput.value;
+    const newDescriptionInput = document.querySelector('#description');
     const description = newDescriptionInput.value;
+    const newAssignedToInput = document.querySelector('#assigned-to');
     const assignedTo = newAssignedToInput.value;
+    const newDueDateInput = document.querySelector('#due-date');
     const dueDate = newDueDateInput.value;
-    if (name === ""){
+    if (name===""){
         $('#name-alert').show()
         setTimeout(()=>{$('#name-alert').hide()},2000)
         return false;
     }
-    if (description === "") {
+    if (description==="") {
         $('#descrip-alert').show()
         setTimeout(()=>{$('#descrip-alert').hide()},2000)
         return false;
     }
-    if (assignedTo === ""){
+    if (assignedTo===""){
         $('#assigned-alert').show()
         setTimeout(()=>{$('#assigned-alert').hide()},2000)
         return false;
@@ -33,11 +30,9 @@ const validFormFieldInput = () => {
         setTimeout(()=>{$('#due-alert').hide()},2000)
         return false;
     }
-    
     taskManager.addTask(name,description,assignedTo,dueDate)
     taskManager.save()
     taskManager.render()
-
     newTaskNameInput.value = ""
     newDescriptionInput.value = ""
     newAssignedToInput.value = ""
@@ -61,4 +56,3 @@ tasksList.addEventListener('click',(event) => {
         return false;
     }
 })
-
